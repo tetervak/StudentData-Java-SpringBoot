d3e8e709ceda597d864f5822d3b120c2799a871d
Alex Tetervak <tetervak@sheridancollege.ca>
2024-01-29 22:26:08 -0500

    removed unused imports

--- src/main/java/ca/tetervak/studentdata/config/HttpSessionConfig.java
+++ src/main/java/ca/tetervak/studentdata/config/HttpSessionConfig.java
@@ -2,8 +2,6 @@ package ca.tetervak.studentdata.config;
 
 import jakarta.servlet.http.*;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
